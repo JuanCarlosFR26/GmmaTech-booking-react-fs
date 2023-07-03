abd2acf503700b75350255d437c971713f0f112a
unknown <jcf.daw@gmail.com>
2023-07-04 01:59:42 +0200

    adding layout, pages and router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Layout from "./layout/Layout";
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
