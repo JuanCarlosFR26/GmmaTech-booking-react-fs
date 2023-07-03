8ac72ce06dbfea05f653fa111d89824ac88c3aa1
unknown <jcf.daw@gmail.com>
2023-07-04 00:32:27 +0200

    preparing pg config and index.js with router

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const PORT = process.env.PORT || 8001;
+const app = express();
+
+const cors = require('cors');
+require('dotenv').config();
+
+const router = require('./routes/routes');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+    console.log(`The Dungeon Master is listening on port ${PORT}`);
+})
