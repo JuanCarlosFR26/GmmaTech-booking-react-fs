ef34af1394a13f01d5e780cc3b53db42c86afb8f
unknown <jcf.daw@gmail.com>
2023-07-05 02:13:14 +0200

    adding sessionstorage to home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { DataUser } from "../context/UserDataProvider";
 import { UsersReservations } from "../context/ReservationsUserProvider";
 
 const Home = () => {
   const { loggedUser } = useContext(DataUser);
   const { reservationsList } = useContext(UsersReservations);
+  const [reservs, setReservs] = useState(null)
+
+  useEffect(() => {
+    setReservs(JSON.parse(sessionStorage.getItem('reservations')))
+  }, [])
 
   return (
     <div className="flex flex-col items-center justify-center mt-20 w-screen">
       <h1 className="font-bold text-white text-4xl bg-userReservation p-2 shadow-md shadow-amber-600 rounded-2xl">Welcome {loggedUser}</h1>
       <div className="w-3/4 flex flex-wrap items-center justify-center gap-6 p-10 font-bold">
-        {reservationsList ? (
-          reservationsList.map((reservation) => (
+        {reservs ? (
+          reservs.map((reservation) => (
             <div className="bg-userReservation text-white flex flex-col p-8 rounded-3xl shadow-xl shadow-amber-700 items-center w-1/4">
               <h2>Reservation {reservation.reservation_id}</h2>
               <p>Starts at {reservation.time_start}</p>
